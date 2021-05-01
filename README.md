bdba5a57236f7b2c51505d562f8be9c0db8aa821
Serhii Stanislav <serhiistanislav@gmail.com>
2021-05-01 21:54:16 +0300

    adds routing and ejs

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - express
 - nodemon
+- ejs
--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { articles } = require('./model/data.json');
 
 app.use(express.static('public'));
+app.use(express.urlencoded());
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index', { title: 'GoIT' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact' });
+});
+
+app.post('/contact', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.get('/blog', (req, res) => {
+  res.render('blog', { articles, title: 'Blog' });
 });
 
 app.listen(port, () => {
